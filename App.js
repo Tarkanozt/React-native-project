f392eb0c16c498f1caf1d74da5c798582d27d891
Tarkan Ozturk <tarkanozt00@gmail.com>
2021-05-29 15:59:16 +0300

    fetch

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { Icon, Text } from 'react-native-elements';
+import Navigation from "./Fetchdata"
 
 export default function App() {
+
+function Hello(){
+
+  alert("Hello User")
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+    <View style={{ flex: 8, padding: 40 }}>
+      <View style={{ flex: 2}}>
+        <Text h1 style={{ textAlign:'center', justifyContent: 'center' }}>Welcome to my App</Text>
+      </View>
+      
+      <View style={{ flex: 1, padding: 10 }}>
+        <Text h4 style={{textAlign:'center', color: 'red' }}> Please choose one option! </Text>
+      </View>
+      {/*<Icon name= 'face'/>*/}
+
+      {/* 1. Box */}
+      <View style={{ flex: 2}}>
+        <TouchableOpacity onPress = {() =>Hello()}>
+        <View style= {styles.button1} >
+        <Text style={styles.buttonText}> Products </Text>
+        </View>
+        </TouchableOpacity>
+    </View>
+
+      {/* 2. Box */}
+      <View style={{ flex: 2}}>
+      <TouchableOpacity onPress = {() =>Hello()}>
+        <View style= {styles.button2} >
+        <Text style={styles.buttonText}> Categories </Text>
+        </View>
+        </TouchableOpacity>
+      </View>
+      {/* 3. Box */}
+      <View style={{ flex: 2}}>
+      <TouchableOpacity onPress = {() =>Hello()}>
+        <View style= {styles.button3} >
+        <Text style={styles.buttonText}> Orders </Text>
+        </View>
+        </TouchableOpacity>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  button1: {
+    borderRadius: 8,
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+    backgroundColor: 'blue',
+    margin:10,
   },
+  button2: {
+    borderRadius: 8,
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+    backgroundColor: 'purple',
+    margin:10,
+  },
+  button3: {
+    borderRadius: 8,
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+    backgroundColor: 'green',
+    margin:10,
+  },
+ buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 20,
+    textAlign: 'center',
+}
 });
